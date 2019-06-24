42a49bd8372b4400bd4784a1352677ab6709ef52
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-06-24 18:52:59 -0500

    auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-danger">Hi</button>
+      <Auth />
+      <Home />
     </div>
   );
 }
