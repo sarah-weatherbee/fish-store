e3531440dd879f395560d1d42e715fe16ce0a013
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-06-24 20:29:09 -0500

    added login

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import Auth from '../components/Auth/Auth';
 import Home from '../components/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+import fbConnection from '../helpers/data/connection';
 
-function App() {
-  return (
-    <div className="App">
-      <Auth />
-      <Home />
-    </div>
-  );
+fbConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const loadComponent = () => {
+      if (this.state.authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    };
+    return (
+        <div className="App">
+          {loadComponent()}
+        </div>
+    );
+  }
 }
+
 
 export default App;
